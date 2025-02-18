86f773e65d96d6868a0b2728b7c07bfa7f5a99b8
Jean-Elie <jeaneliekubembula@gmail.com>
2025-02-18 16:43:29 +0100

    implementation des url

--- mossala/urls.py
+++ mossala/urls.py
@@ -15,8 +15,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from mossala import settings
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API de Mossala",
+      default_version='v0.0.1', 
+      description="Documentation de l'API de Mossala",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="eneogroug.cg@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
