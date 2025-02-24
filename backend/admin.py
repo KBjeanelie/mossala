bd517001d2c11a78226e86192cb6340ed69c7457
Jean-Elie <jeaneliekubembula@gmail.com>
2025-02-24 16:12:27 +0100

    imgration de backend

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models.domain_manager import Domain, Job, Specialty
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ['name', 'description']
+    list_filter = ['created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+    ordering = ['name']
+    save_on_top = True
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'domain', 'description', 'created_at', 'updated_at')
+    search_fields = ['name', 'domain__name', 'description']
+    list_filter = ['domain', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+    ordering = ['name']
+    save_on_top = True
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'specialty', 'description', 'created_at', 'updated_at')
+    search_fields = ['title', 'specialty__name', 'description']
+    list_filter = ['specialty', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+    ordering = ['title']
+    save_on_top = True
+    readonly_fields = ['created_at', 'updated_at']
