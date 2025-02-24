ec4ecd0167ad27d4389db937de4debf77653d0bb
Jean-Elie <jeaneliekubembula@gmail.com>
2025-02-24 16:54:57 +0100

    ajout des permission d'authentication dans le fichier domain_manager_views.py

--- api/views/domain_manager_views.py
+++ api/views/domain_manager_views.py
@@ -1,17 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from backend.models.domain_manager import Domain, Specialty, Job
 from api.serializers.domain_manger_serializer import DomainSerializer, SpecialtySerializer, JobSerializer
 
 class DomainViewSet(viewsets.ModelViewSet):
     queryset = Domain.objects.all()
     serializer_class = DomainSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class SpecialtyViewSet(viewsets.ModelViewSet):
     queryset = Specialty.objects.all()
     serializer_class = SpecialtySerializer
+    permission_classes = [IsAuthenticated]
     
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    permission_classes = [IsAuthenticated]
